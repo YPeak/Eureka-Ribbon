845040db38fee3780bb4efd622f163b0f9d1c8df
yu <yushuwei178@163.com>
2018-05-18 17:37:39 +0800

    添加dashboard--监控可视化界面

--- dashboard/src/main/java/com/example/dashboard/DashboardApplication.java
+++ dashboard/src/main/java/com/example/dashboard/DashboardApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
-@EnableHystrixDashboard
+@EnableHystrixDashboard //可视化监控界面注解
 public class DashboardApplication {
 
 	public static void main(String[] args) {
